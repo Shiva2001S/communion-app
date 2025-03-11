e184cdb938cab45e95502cb268287c5bb82fdd65
Shiva <shiva66282kumar@gmail.com>
2025-03-11 21:26:35 +0530

    error fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from '../styles/Card.module.css';
-const Card = ({title, desc, date, loc, cat}) => {
+const Card = ({ title, desc, date, loc, cat }) => {
   return (
     <div className={styles.first}>
-        <h2>{title}</h2>
-        <p>{desc}</p>
+      <h2>{title}</h2>
+      <p>{desc}</p>
+      <div>
         Date : <h3>{date}</h3>
+      </div>
+      <div>
         Location : <h3>{loc}</h3>
-        <h3>{cat}</h3>
+      </div>
+      <h3>{cat}</h3>
     </div>
   )
 }
